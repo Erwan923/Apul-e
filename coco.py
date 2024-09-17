b9fa2d30f674c13661363cb40584e9d947a7b97c
Erwan923 <erwan.billard@protonmail.com>
2024-09-17 15:27:20 +0000

    Ajout du fichier coco.py généré par Codestral

---
+++ coco.py
@@ -0,0 +1,7 @@
+comme `https://www.youtube.com/watch?v=QRYQm-66FqI`
+
+Il y a plusieurs façons de télécharger des vidéos à partir d'URLs, mais il est important de noter que le téléchargement de vidéos à partir de sites comme YouTube peut violer les conditions d'utilisation de ces sites. Il est toujours préférable de vérifier les règles et les lois en vigueur dans votre région avant de procéder.
+
+Cependant, si vous souhaitez télécharger une vidéo YouTube, vous pouvez utiliser l'API YouTube Data ou des outils en ligne comme SaveFrom.Net. Cependant, ces méthodes nécessitent une compréhension technique ou un outil prêt à l'emploi.
+
+Si vous cherchez une solution de programmation,
